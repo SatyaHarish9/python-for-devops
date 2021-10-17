a9ea4ce8f824353a27d16c0545073a99f8703ba9
SatyaHarish9 <harishgvt@gmail.com>
2021-10-17 15:45:38 -0400

    checking file using isfile() method

---
+++ isfile.py
@@ -0,0 +1,9 @@
+import os.path
+from os import path
+
+def main():
+    print("Is it File? " + str(path.isfile('send-email.py')))
+    print("Is it File? " + str(path.isfile('python-for-devops')))
+
+if __name__== "__main__":
+    main()
