7887772a4e9daf0c0ea28d34f5df73679ed8e80b
SatyaHarish9 <harishgvt@gmail.com>
2021-10-29 14:59:01 -0500

    adding the sample program to demo getpass module

--- password.py
+++ password.py
@@ -4,3 +4,6 @@ my_pass=getpass.getpass(prompt="Enter your password: ")
 
 # To verify the password
 #print("Entered password is:", my_pass)
+
+user=getpass.getuser()
+print("Current user logged in is:", user)
