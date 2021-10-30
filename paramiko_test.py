72df370010c5aebc3be2c65ca38c7c2f70c4e4c4
SatyaHarish9 <harishgvt@gmail.com>
2021-10-30 15:42:00 -0500

    paramiko module for SSH

---
+++ paramiko_test.py
@@ -0,0 +1,13 @@
+import paramiko
+import time
+import os
+
+password=os.environ.get('PASSWORD')
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(hostname='44.X.X.X',username='centos',password=password)
+stdin,stdout,stderr = ssh.exec_command('free -m')
+time.sleep(5)
+print(stdout.readlines())
+ssh.close()
